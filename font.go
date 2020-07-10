3d0a7c10b1ca8641543ad2cc8bcfdba5d28aafe7
Devlyn <Dev@Github1.DKS>
2020-07-10 13:33:57 +0200

    GLF1: [Updated openGL version] Increased version to v3.3-compatibility

--- font.go
+++ font.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/go-gl/gl/v2.1/gl"
+	"github.com/go-gl/gl/v3.3-compatibility/gl"
 )
 
 // Direction represents the direction in which strings should be rendered.
